76050bb4d47d847a9f23f3bdb41e1806a2d1393d
beattyg2 <garrett@gbt.codes>
2019-03-14 14:59:36 -0400

    fixed bugs

--- web/server.py
+++ web/server.py
@@ -6,7 +6,6 @@ from PIL import Image
 from flask import Flask, Response
 from flask import render_template
 from flask import request
-
 from steganography.message import Message
 from steganography.source import Source
 from steganography.steganography import Steganography
@@ -16,6 +15,7 @@ app = Flask(__name__)
 if __name__ == ' __main__':
     app.debug = True
     app.run(host='0.0.0.0')
+
 
 def get_base_64_str(image):
     in_mem_file = io.BytesIO()
@@ -32,21 +32,21 @@ def get_base_64_str(image):
 @app.route('/encode', methods=['POST'])
 def encode():
     source_image = request.files.get('source')
-    m = request.files.get('message')
+    message = request.files.get('message')
     bit_split = int(request.form['bitsplit'])
 
-    if not source_image or not m or not bit_split:
+    if not source_image or not message or not bit_split:
         return Response(response=json.dumps(obj='Source and Message not uploaded.'), status=400,
                         mimetype='application/json')
 
     try:
-        source = Source(source_image, source_type='image')
-        try:
-            message = Message(m, message_type='image')
-        except:
-            message = Message(m, message_type='text_stream')
 
-        encoded = Steganography.encode(source, message, bit_split)
+        try:
+            encoded = Steganography.encode(source_image, message, bit_split, source_type='image', message_type='image')
+        except:
+            encoded = Steganography.encode(source_image, message, bit_split, source_type='image',
+                                           message_type='text_stream')
+
         image_encoded = Image.fromarray(encoded)
         data = {'message': get_base_64_str(image_encoded)}
         resp = Response(response=json.dumps(data), status=200, mimetype='application/json')
@@ -62,14 +62,15 @@ def encode():
 def decode():
     source_image = request.files.get('source')
     if source_image:
-        source = Source(source_image, source_type='image')
-        message, message_type, extras = Steganography.decode(source)
-        message = Message.convert_to_type(message, message_type, extras)
+        message, message_type, extras = Steganography.decode(source_image, source_type='image')
 
         if message_type == 'image':
-            data = {'message': get_base_64_str(message), 'secret_type':message_type}
-        else:
-            data = {'message': message, 'secret_type':message_type}
+            message = message.reshape(extras)
+            message = Image.fromarray(message)
+            data = {'message': get_base_64_str(message), 'secret_type': message_type}
+        elif message_type == 'text':
+            decoded_text = ''.join(chr(x) for x in message)
+            data = {'message': decoded_text, 'secret_type': message_type}
         resp = Response(response=json.dumps(data), status=200, mimetype='application/json')
         return resp
     return Response(response=json.dumps(obj='Source not uploaded'), status=400, mimetype='application/json')
